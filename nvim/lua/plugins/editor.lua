45af5683824f09883f30dce7d6f7ea57585d2332
Ek Shoonya <ekshoonya@Eks-Mac-mini.local>
2024-01-07 13:08:39 +0530

    added lazy vim

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,60 @@
+return {
+  "telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  keys = {
+    "<leader>fP",
+    function()
+      require("telescope.builtin").find_files({
+        cwd = require("lazy.core.config").options.root,
+      })
+    end,
+    desc = "Find Plugin File",
+  },
+  {
+    ";f",
+    function()
+      local builtin = require("telescope.builtin")
+      builtin.find_files({
+        no_ignore = false,
+        hidden = true,
+      })
+    end,
+  },
+  {
+    ";r",
+    function()
+      local builtin = require("telescope.builtin")
+      builtin.live_grep()
+    end,
+  },
+  {
+    ";t",
+    function()
+      local builtin = require("telescope.builtin")
+      builtin.help_tags()
+    end,
+  },
+  {
+    "\\\\",
+    function()
+      local builtin = require("telescope.builtin")
+      builtin.buffers()
+    end,
+  },
+  {
+    ";;",
+    function()
+      local builtin = require("telescope.builtin")
+      builtin.resume()
+    end,
+  },
+  ";e",
+  {
+    function()
+      local builtin = require("telescope.builtin")
+      builtin.diagnostics()
+    end,
+  },
+}
